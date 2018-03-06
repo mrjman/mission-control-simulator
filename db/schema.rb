55b1e8d2b21ef1120380476b098f1a4cb931286a
Jesse <jesse@mondorobot.com>
2018-03-06 08:24:31 -0700

    Schema updates from running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304201627) do
+ActiveRecord::Schema.define(version: 20180306150224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180304201627) do
     t.string "access_token_scope"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tokens_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,9 +39,7 @@ ActiveRecord::Schema.define(version: 20180304201627) do
     t.float "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "token_id"
-    t.index ["token_id"], name: "index_users_on_token_id"
   end
 
-  add_foreign_key "users", "tokens"
+  add_foreign_key "tokens", "users"
 end
